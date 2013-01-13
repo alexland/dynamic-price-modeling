f0fe95db357015f9931a17c154546c0fbff50ad0
doug y'barbo <doug.ybarbo@gmail.com>
2013-01-12 21:04:13 -0800

    various revisions to generate iso-quants

--- news-vendor.py
+++ news-vendor.py
@@ -17,16 +17,16 @@ mu, sigma = 100, 10
 max_trials = 500
 
 
-# calculation of a range of inventory levels, varying mean demand
-for inventory_level in xrange(mu - 5*sigma, mu + 5*sigma):
+# calculate a range of inventory levels, as a function of mean demand
+for inventory_level in xrange(mu - 5 * sigma, mu + 5*sigma):
 	revenue_mean = 0
 	for trial in xrange(max_trials):
 		# demand for each day
-		demand = int(.5 + gauss(mu, sigma))
+		demand = int(.5 + NP.random.normal(mu, sigma, 1))
 		# revenue for each day
 		# number of units sold is the lesser of inventory &
 		# demand, t/4 revenue = num units sold x sales price
-		revenue = item_sale_price*NP.min(inventory_level, demand) - item_cost*inventory_level
+		revenue = isp * NP.min(inventory_level, demand) - ic * inventory_level
 		revenue_mean += revenue
 
 # print("{0}\t{1}\t{2}".format(c1, n, avg/max_trials))
@@ -36,8 +36,9 @@ def revenue(isp, mu, s, mt, ic):
 	"""
 	returns mean revenue, a 1D vector w/ len equal to
 	value of 'mt';
-	pass in inventory level, mu & sigma, max_trials for demand
-	distribution, and item's cost and sale price
+	pass in item sale price, mu & sigma, max_trials or mt
+	for demand distribution, & item's cost; 
+	isp is a 1D vector, all others are scalars
 	"""
 	il = NP.linspace(mu - 5*s, mu + 5*s, mt)
 	demand = NP.random.normal(loc=mu, scale=s, size=mt)
@@ -51,7 +52,10 @@ def revenue(isp, mu, s, mt, ic):
 # generate multiple (rev vs inv) curves by varying the item sale price, isp:
 isp = NP.arange(1.5, 6, .5)
 
+# all arguments in origial 'revenue' fn are frozen except for isp, which still must be passed in
 revenue2 = partial(revenue, mu=100, s=10, mt=10, ic=1.)
+
+res = [ revenue2(isp) for isp in NP.arange(1.5, 6., .5) ]
 
 
 #------------- plotting -----------------#
